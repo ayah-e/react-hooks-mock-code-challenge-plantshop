ea0a9897d50ca632fbcf0f39249ffd6788482e64
Ayah Elsais <elsais.ayah@gmail.com>
2023-02-08 09:54:23 -0500

    rendered plants on page

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,19 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import Header from "./Header";
 import PlantPage from "./PlantPage";
 
 function App() {
+const [plants, setPlants] = useState([]);
+useEffect(() => {
+  fetch("http://localhost:6001/plants")
+  .then(response => response.json())
+  .then(plants => setPlants(plants));
+},[]);
+// console.log(plants); nice got the plants data set to plants array
   return (
     <div className="app">
       <Header />
-      <PlantPage />
+      <PlantPage plants = {plants} />
     </div>
   );
 }
