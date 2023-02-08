83853cea3ce61ba4d46e4d93a208d06b040a6000
Ayah Elsais <elsais.ayah@gmail.com>
2023-02-08 10:02:55 -0500

    toggle in/out of stock button

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,16 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 
 function PlantCard({key, name, image, price}) {
+  const [soldOut, setSoldOut] = useState(true);
+  function handleSoldOut() {
+    setSoldOut(soldOut =>!soldOut);
+  }
   return (
     <li className="card">
       <img src={image} alt={name} />
       <h4>{name}</h4>
       <p>Price: {price}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
+
+      {soldOut ? (
+        <button className="primary" onClick = {handleSoldOut}>In Stock</button>
       ) : (
-        <button>Out of Stock</button>
+        <button onClick = {handleSoldOut}>Out of Stock</button>
       )}
+
     </li>
   );
 }
